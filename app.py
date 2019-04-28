61aaf1be2531ce932463563d5123ba0b4a458673
Grace Thompson <grace.r.thompson@disney.com>
2019-04-27 19:57:41 -0700

    updated response

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def main():
     return "Welcome!"
 
-@app.route('/how are you')
+@app.route('/howdy')
 def hello():
-    return 'I am good, how about you?'
+    return 'You know, cant complain'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
